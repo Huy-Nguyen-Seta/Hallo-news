0fd6002f6e00d37cb5ceda5455ebee15d6f76ca8
Nguyen Huy Seta <nguyenquanghuy06102000@gmail.com>
2023-12-22 00:45:10 +0700

    feat(cms-153):admin for service develop

---
+++ src/api/services-our-sourcing/routes/services-our-sourcing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * services-our-sourcing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::services-our-sourcing.services-our-sourcing');
