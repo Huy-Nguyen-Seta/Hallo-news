c2528e08d26b5399043a28bf73fd0520116c11b2
Nguyen Huy Seta <nguyenquanghuy06102000@gmail.com>
2024-01-10 06:22:50 +0700

    update footer

--- src/api/footer/controllers/footer.js
+++ src/api/footer/controllers/footer.js
@@ -9,13 +9,12 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::footer.footer", ({ strapi }) => ({
   async getFooter(ctx) {
     const query = {
-      populate: ["Footer.items", "SubFooter.items"],
+      populate: ["Footer.items", "FooterBottom"],
     };
     const footer = await strapi.entityService.findMany(
       "api::footer.footer",
       query
     );
-    console.log("footer", footer);
     // const sanitizedEntity = await this.sanitizeOutput(footer, ctx);
     return { data: footer };
   },
