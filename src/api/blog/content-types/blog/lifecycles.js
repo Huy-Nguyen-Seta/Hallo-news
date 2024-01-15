1bd17b927886a865c47b7137cc6a0369f1781673
Nguyen Huy Seta <nguyenquanghuy06102000@gmail.com>
2024-01-15 12:55:42 +0700

    update issue: 14/1

---
+++ src/api/blog/content-types/blog/lifecycles.js
@@ -0,0 +1,36 @@
+function toLowerCaseNonAccentVietnamese(str) {
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a');
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e');
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i');
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o');
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u');
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y');
+  str = str.replace(/đ/g, 'd');
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ''); // Huyền sắc hỏi ngã nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ''); // Â, Ê, Ă, Ơ, Ư
+  return str;
+}
+ const handleTextToUniqueId = (text) => {
+  if (text) {
+    let newText = toLowerCaseNonAccentVietnamese(text);
+    newText = newText.replace(/[^a-zA-Z0-9 ]/g, '');
+    newText = newText.replaceAll(' ', '-');
+    return newText;
+  } else {
+    return '';
+  }
+};
+module.exports = {
+  beforeCreate(event) {
+    const { data, where, select, populate } = event.params;
+    console.log("event.params.data.title", event.params.data.title);
+    event.params.data.slug = handleTextToUniqueId(event.params.data.title);
+  },
+  afterCreate(event) {
+    const { result, params } = event;
+
+    // do something to the result;
+  },
+};
